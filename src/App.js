1ef5bac55ffc2392a686062d9dd5261ba9a67e95
Yeongseon Choe <yeongseon.choe@gmail.com>
2023-07-19 17:26:13 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState('');  
+  const [data, setData] = useState('');
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch('/api/HttpTrigger1')).json();
-      console.log(text);
-      setData(text);
+    (async function fetchData() {
+      try {
+        const response = await fetch('/api/HttpTrigger1');
+        if (!response.ok) {
+          throw new Error('API request failed');
+        }
+        const { text } = await response.json();
+        console.log(text);
+        setData(text);
+      } catch (error) {
+        setError(error.message);
+      }
     })();
-  });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return <div>Hello {data}</div>;
 }
