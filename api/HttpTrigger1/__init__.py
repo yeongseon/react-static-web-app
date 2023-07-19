3054c5e04928fcf846c9a2ad52dceeab72995b23
Yeongseon Choe <yeongseon.choe@gmail.com>
2023-07-19 17:26:02 +0900

    Update api

--- api/HttpTrigger1/__init__.py
+++ api/HttpTrigger1/__init__.py
@@ -7,4 +7,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    return func.HttpResponse(json.dumps({"text": "from the API"}), status_code=200)
+    return func.HttpResponse(
+        json.dumps({"text": "from the API"}),
+        status_code=200,
+        mimetype="application/json",
+    )
